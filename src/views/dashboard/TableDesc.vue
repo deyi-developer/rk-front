cdbf0879b88849be19807012b7ac429f95f5f86d
dingzijian <414603173@qq.com>
2022-03-30 20:03:03 +0800

    概览

--- src/views/dashboard/TableDesc.vue
+++ src/views/dashboard/TableDesc.vue
@@ -3,29 +3,49 @@
     <el-col :span="12">
       <el-card shadow="always" class="box-card">
         <el-descriptions :column="2" title="基础指标" border>
-          <el-descriptions-item label="PJTD含税总收入">{{
-            data.pjtdTotalMoney
-          }}</el-descriptions-item>
-          <el-descriptions-item label="总开票">{{
-            data.totalBilling
-          }}</el-descriptions-item>
-          <el-descriptions-item label="相对PJTD开票率"
-            >{{ billingRateOfTotalPjtd }}%</el-descriptions-item
+          <el-descriptions-item
+            :contentStyle="{ textAlign: 'right' }"
+            label="PJTD含税总收入"
+            >{{ data.pjtdTotalMoney }}</el-descriptions-item
+          >
+          <el-descriptions-item
+            :contentStyle="{ textAlign: 'right' }"
+            label="总开票"
+            >{{ data.totalBilling }}</el-descriptions-item
+          >
+          <el-descriptions-item
+            :contentStyle="{ textAlign: 'right' }"
+            label="相对PJTD开票率"
+            >{{ data.billingRateOfTotalPjtd }}%</el-descriptions-item
           >
 
-          <el-descriptions-item label="总收款">{{
-            data.totalReceipts
-          }}</el-descriptions-item>
-          <el-descriptions-item label="相对PJTD收款率"
+          <el-descriptions-item
+            :contentStyle="{ textAlign: 'right' }"
+            label="总收款"
+            >{{ data.totalReceipts }}</el-descriptions-item
+          >
+          <el-descriptions-item
+            :contentStyle="{ textAlign: 'right' }"
+            label="相对PJTD收款率"
             >{{ data.receivedRateOfTotalPjtd }}%</el-descriptions-item
           >
-          <el-descriptions-item label="相对总开票收款率"
+          <el-descriptions-item
+            :contentStyle="{ textAlign: 'right' }"
+            label="相对总开票收款率"
             >{{ data.totalBillingRate }}%</el-descriptions-item
           >
-          <el-descriptions-item label="-">-</el-descriptions-item>
-          <el-descriptions-item label="-">-</el-descriptions-item>
-          <el-descriptions-item label="-">-</el-descriptions-item>
-          <el-descriptions-item label="-">-</el-descriptions-item>
+          <el-descriptions-item :contentStyle="{ textAlign: 'right' }" label="-"
+            >-</el-descriptions-item
+          >
+          <el-descriptions-item :contentStyle="{ textAlign: 'right' }" label="-"
+            >-</el-descriptions-item
+          >
+          <el-descriptions-item :contentStyle="{ textAlign: 'right' }" label="-"
+            >-</el-descriptions-item
+          >
+          <el-descriptions-item :contentStyle="{ textAlign: 'right' }" label="-"
+            >-</el-descriptions-item
+          >
         </el-descriptions>
       </el-card>
     </el-col>
@@ -33,35 +53,55 @@
       <el-card class="box-card">
         <el-row>
           <el-descriptions :column="2" title="基于账期指标" border>
-            <el-descriptions-item label="总应开金额">{{
-              data.totalShouldBillingMoney
-            }}</el-descriptions-item>
-            <el-descriptions-item label="总已开金额">{{
-              data.totalAlreadyBillingMoney
-            }}</el-descriptions-item>
-            <el-descriptions-item label="总应开未开">{{
-              data.totalShouldNotBillingMoney
-            }}</el-descriptions-item>
+            <el-descriptions-item
+              :contentStyle="{ textAlign: 'right' }"
+              label="总应开金额"
+              >{{ data.totalShouldBillingMoney }}</el-descriptions-item
+            >
+            <el-descriptions-item
+              :contentStyle="{ textAlign: 'right' }"
+              label="总已开金额"
+              >{{ data.totalAlreadyBillingMoney }}</el-descriptions-item
+            >
+            <el-descriptions-item
+              :contentStyle="{ textAlign: 'right' }"
+              label="总应开未开"
+              >{{ data.totalShouldNotBillingMoney }}</el-descriptions-item
+            >
 
-            <el-descriptions-item label="相对应开开票率">{{
-              data.correspondingBillingRate
-            }}</el-descriptions-item>
-            <el-descriptions-item label="总应收金额">{{
-              data.totalShouldReceiptsMoney
-            }}</el-descriptions-item>
-            <el-descriptions-item label="总已收金额">{{
-              data.totalReceiptssMoney
-            }}</el-descriptions-item>
-            <el-descriptions-item label="总应收未收（相对应收）">{{
-              data.totalShouldNotReceiptsMoney
-            }}</el-descriptions-item>
-            <el-descriptions-item label="总应收未收（相对已开）">{{
-              data.totalShouldNotReceiptsBilling
-            }}</el-descriptions-item>
-            <el-descriptions-item label="相对应收收款率"
+            <el-descriptions-item
+              :contentStyle="{ textAlign: 'right' }"
+              label="相对应开开票率"
+              >{{ data.correspondingBillingRate }}</el-descriptions-item
+            >
+            <el-descriptions-item
+              :contentStyle="{ textAlign: 'right' }"
+              label="总应收金额"
+              >{{ data.totalShouldReceiptsMoney }}</el-descriptions-item
+            >
+            <el-descriptions-item
+              :contentStyle="{ textAlign: 'right' }"
+              label="总已收金额"
+              >{{ data.totalReceiptssMoney }}</el-descriptions-item
+            >
+            <el-descriptions-item
+              :contentStyle="{ textAlign: 'right' }"
+              label="总应收未收（相对应收）"
+              >{{ data.totalShouldNotReceiptsMoney }}</el-descriptions-item
+            >
+            <el-descriptions-item
+              :contentStyle="{ textAlign: 'right' }"
+              label="总应收未收（相对已开）"
+              >{{ data.totalShouldNotReceiptsBilling }}</el-descriptions-item
+            >
+            <el-descriptions-item
+              :contentStyle="{ textAlign: 'right' }"
+              label="相对应收收款率"
               >{{ data.relativeReceivableRate }}%</el-descriptions-item
             >
-            <el-descriptions-item label="相对已开收款率"
+            <el-descriptions-item
+              :contentStyle="{ textAlign: 'right' }"
+              label="相对已开收款率"
               >{{ data.relativeOpenedCollectionRate }}%</el-descriptions-item
             >
           </el-descriptions>
@@ -85,3 +125,4 @@ defineProps({
   margin-bottom: 10px;
 }
 </style>
+<style lang="scss"></style>
