fddc4606d05d1cdf065d5349e1fda7f362b1c136
dingzijian <414603173@qq.com>
2022-04-08 16:39:53 +0800

    48配置

---
+++ scp2-prod.js
@@ -0,0 +1,24 @@
+const scp = require("scp2");
+const ora = require("ora");
+const chalk = require("chalk"); //设置命令颜色的
+const spinner = ora(chalk.blue("正在部署到服务器...")); //显示加载
+
+spinner.start();
+scp.scp(
+  "./dist/",
+  {
+    host: "172.22.3.48", // 服务器的地址
+    port: 22, // 服务器端口， 一般为 22
+    username: "harry", // 用户名
+    password: "uI3rnw%Q0*D60g^Y",
+    path: "/usr/project/www/rk/" //服务器存放文件路径
+  },
+  (err) => {
+    if (!err) {
+      console.log(chalk.green(`部署完成!`));
+    } else {
+      console.log(chalk.red(`部署失败!`));
+    }
+    spinner.stop();
+  }
+);
