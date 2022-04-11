4c93b0f668148cc7ae445c3a6040dda3e76b5caa
dingzijian <414603173@qq.com>
2022-04-11 09:59:18 +0800

    shell

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+ 
+ # file to upload
+FOLDER_NAME="./dist"
+WORK_DIR_TEST="/root/opt/hydemoweb"
+
+echo "*******Start to build*******"
+npm run build:prod
+
+echo "*******Start to deploy *******"
+
