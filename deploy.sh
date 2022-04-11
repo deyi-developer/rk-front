b005d83631189b5d38ca58484533f0e86a2f457f
dingzijian <414603173@qq.com>
2022-04-11 10:31:28 +0800

    部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,3 @@
- 
- # file to upload
-INPUT_FOLDER="/usr/project/www/rk/"
-OUT_FOLDER="/root/guns-vip-admin/data/rk-system/rk/"
 
-echo "*******Start to cpoy*******"
-rm -rf OUT_FOLDER
-cp -r INPUT_FOLDER OUT_FOLDER
-
-echo "*******end*******"
-
+rm -rf /root/guns-vip-admin/data/rk-system/rk/
+cp -r /usr/project/www/rk/ /root/guns-vip-admin/data/rk-system/rk/
