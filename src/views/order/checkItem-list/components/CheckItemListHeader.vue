a807574e703a4d7325d0f869650cc98cca00165a
阚细康 <xikang.han@hand-china.com>
2022-08-25 09:24:12 +0800

    新增checkItem菜单

---
+++ src/views/order/checkItem-list/components/CheckItemListHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="checkItem-header">
+    <el-date-picker v-model="yearMonth" type="month" placeholder="选择月">
+    </el-date-picker>
+    <vxe-button @click="exportDataEventEmit">默认导出</vxe-button>
+  </div>
+</template>
+<script>
+export default {
+  props: ["yearMonth", "exportDataEvent"],
+  methods: {
+    exportDataEventEmit() {
+      this.$emit("exportDataEvent");
+    },
+  },
+};
+</script>
